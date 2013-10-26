08db1d5bdab94ee243814e941b2d15a4c3f2c802
Jarbsku <jarno.lahti@metropolia.fi>
2013-10-26 12:59:57 +0300

    meteor testing

--- SpaceDuds/src/GameObjects/Meteor.java
+++ SpaceDuds/src/GameObjects/Meteor.java
@@ -11,13 +11,16 @@ public class Meteor extends GameObject {
     }
     
     private void meteorShape(){
-        super.shapeVecCount = 4;
-        super.graphicsVecCount = 6;
-        super.shape[0] = new Vec2(-1f,1f);
-        super.shape[1] = new Vec2(-1f,-1f);
-        super.shape[2] = new Vec2(1f,-1f);
-        super.shape[3] = new Vec2(1f,1f);
-        super.shape[4] = new Vec2(-1f,1f);
-        super.shape[5] = new Vec2(1f,-1f);
+        super.shapeVecCount = 8;
+        super.graphicsVecCount = 9;
+        super.shape[0] = new Vec2(0f,1f);
+        super.shape[1] = new Vec2(-1f,0f);
+        super.shape[2] = new Vec2(-0.5f,-0.5f);
+        super.shape[3] = new Vec2(0f,1f);
+        super.shape[4] = new Vec2(-0.5f,-1f);
+        super.shape[5] = new Vec2(0.4f,-0.9f);
+        super.shape[6] = new Vec2(0f,1f);
+        super.shape[7] = new Vec2(0.5f,-0.4f);
+        super.shape[8] = new Vec2(0.5f,0.5f);
     } 
 }
